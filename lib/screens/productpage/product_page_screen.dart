3d3cd1c9e78dbfb755120112ff49dab9e5fc7f3f
Flaudin <2016-1362751@asiancollege.edu.ph>
2023-05-14 11:52:51 +0800

    Removing API key

--- lib/screens/productpage/product_page_screen.dart
+++ lib/screens/productpage/product_page_screen.dart
@@ -6,6 +6,15 @@ class ProductPageScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Product Page",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
   }
 }
